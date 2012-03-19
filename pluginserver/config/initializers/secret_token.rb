674b96e62255ebd3f7a2f7804d6e8727a8befda3
= <=>
2012-03-19 11:28:07 -0700

    Initial commit

---
+++ pluginserver/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pluginserver::Application.config.secret_token = '88be4cfcad35ddc3de912af3419cfbb742d6b474893cc07bc56bc0e5c2612df9616a015b7a35467611f9b390a4c78a3017c92a6cf74e37e2bf998d572b6a83a5'
